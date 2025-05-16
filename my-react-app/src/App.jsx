2da679f1311e437918a4cbd2474081356105f50f
SaahilTakooree <meharsingtakooree7@gmail.com>
2025-05-16 16:09:54 +0400

    Added a custom cursor component.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Login_Signup from './Login-Signup.jsx';
 import ForgetPassword from './ForgetPassword.jsx';
+import Cursor from './Cursor'; 
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login_Signup />} />
-        <Route path="/forgot-password" element={<ForgetPassword />} />
-      </Routes>
-    </Router>
+    <>
+      <Cursor />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login_Signup />} />
+          <Route path="/forgot-password" element={<ForgetPassword />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 

5d81cd151f64af8ae9cee11790ebc69123a9ddb9
SaahilTakooree <meharsingtakooree7@gmail.com>
2025-05-18 07:35:31 +0400

    fixes #2

--- my-react-app/src/AddPost.jsx
+++ my-react-app/src/AddPost.jsx
@@ -12,7 +12,8 @@ function AddPost() {
     const [ section, setSection] = useState(0);
 
     return (
-        <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
+        <div className="fixed top-0 left-0 w-screen h-screen overflow-hidden">
+
             <Canvas shadows camera={{ position: [3, 3, 3], fov: 20 }}>
                 <ScrollControls pages={4} damping={0.1}>
                 <ScrollManager section={section} onSectionChange={setSection} />
